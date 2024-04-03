83ed7eba63d5d1e7403dbe8c3ec3c601ab6a7b35
Daniel Polk <daniel.polk@microsoft.com>
2024-04-03 11:22:52 -0500

    Add files via upload
    A script to delete stale resources in an Azure Subscription (resource that have not been changed in specified time window)

---
+++ DeleteStaleResourcesGit.ps1
@@ -0,0 +1,30 @@
+﻿# Connect to Azure az.accounts
+Connect-AzAccount
+
+# Select the subscription
+$subscriptionId = " "
+Select-AzSubscription -SubscriptionId $subscriptionId
+
+$token = (Get-AzAccessToken -ResourceUrl 'https://management.azure.com').Token
+
+# Define headers for API requests
+$Headers = @{
+    'Authorization' = "Bearer $token"
+}
+
+# Get all resources and their changed time
+$resources = Invoke-RestMethod -Uri "https://management.azure.com/subscriptions/$subscriptionId/resources?api-version=2020-01-01&\$expand=changedTime,createdTime" -Method Get -Headers $Headers | Select-Object -ExpandProperty value
+
+# Delete resources not changed in the last 120 days
+foreach ($resource in $resources) {
+    $time = [datetime]::Parse($resource.changedTime)
+    $datetime = (Get-Date).AddDays(-120)
+    $utcDatetime = $datetime.ToUniversalTime()
+
+    if ($time -lt $utcDatetime) {
+        $resource.id
+        $time
+        Write-Output "Deleting resource now"
+        Remove-AzResource -ResourceId $resource.id -Force
+    }
+}
